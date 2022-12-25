96fc56719d8bbc88ab2d48050ba62ab9d1178cb2
Nick <k.kolabis@mail.ru>
2022-12-25 18:19:29 +0300

    edits

---
+++ 2.py
@@ -0,0 +1,9 @@
+def sieve_of_eratosthenes(number: int):
+    row = {i: '_' for i in range(2, number + 1)}
+    for i in row:
+        pass
+    print(row)
+
+
+if __name__ == '__main__':
+    sieve_of_eratosthenes(10)
