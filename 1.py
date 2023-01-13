73d2ff4d96ffef31ebb3a22761168f358d1ab5c3
Nick <k.kolabis@mail.ru>
2023-01-13 22:12:32 +0300

    preparing...

--- 1.py
+++
@@ -1,8 +0,0 @@
-def bbp_formula(d):
-    return sum(
-        [(1 / 16 ** i) * (4 / (8 * i + 1) - 2 / (8 * i + 4) - 1 / (8 * i + 5) - 1 / (8 * i + 6)) for i in
-         range(int(1 / d))])
-
-
-if __name__ == '__main__':
-    print(bbp_formula(0.0001))
