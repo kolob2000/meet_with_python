ae152d0109ccfcc269a995bedf148710eb5a3f26
Nick <k.kolabis@mail.ru>
2023-02-01 12:14:35 +0300

    add tictactoe

--- tic_tac_toe/game_func.py
+++ tic_tac_toe/game_func.py
@@ -1,12 +1,15 @@
 def check_desk(desk: list, symbol: str) -> bool:
     for i in range(3):
+        print(desk[i])
         if desk[i].count(symbol) == 3:
             return True
         elif [desk[0][i], desk[1][i], desk[2][i]].count(symbol) == 3:
             return True
         elif [desk[0][0], desk[1][1], desk[2][2]].count(symbol) == 3:
             return True
-        return False
+        elif [desk[0][2], desk[1][1], desk[2][0]].count(symbol) == 3:
+            return True
+    return False
 
 
 def reset_board(board: list):
--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -3,19 +3,20 @@ import time
 from asyncio import sleep
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, Text
+from aiogram.filters import Command, Text, Filter
 from aiogram.types import Message, ContentType, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
+from tic_tac_toe.filter import IsMoveFilter, LetsPlayFilter, DontPlayFilter
 from tic_tac_toe.game_func import check_desk, reset_board
 from tic_tac_toe.keyboard import create_keyboard
 
-API_TOKEN: str = '5662299299:AAFUectBCIxYz6die5bjaXnC04Heywo9qbY'
+API_TOKEN: str = 'YOUR_TOKEN'
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 board: list = [['.' for _ in range(3)] for _ in range(3)]
 
-print(board[int('0')])
 move = ['X']
+count = [0]
 
 
 @dp.message(Command(commands=["start"]))
@@ -28,8 +29,10 @@ async def process_start_command(message: Message):
 # Этот хэндлер будет срабатывать на команду "/help"
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ '
-                         'я пришлю тебе твое сообщение')
+    await message.answer('Чтобы сыграть введите любое сообщение \n'
+                         '[ok, yes, да, ок, сыграем, давай]\n'
+                         'либо команду /game.\n'
+                         'для выхода из игры команда /cancel.')
 
 
 @dp.message(Command(commands=['game']))
@@ -38,21 +41,44 @@ async def process_help_command(message: Message):
                          reply_markup=create_keyboard(board))
 
 
-@dp.callback_query()
-async def button_1(callback: CallbackQuery, bot: Bot):
+@dp.message(DontPlayFilter())
+async def process_help_command(message: Message):
+    time.sleep(1)
+    reset_board(board)
+    count[0] = 0
+    move[0] = 'X'
+    await message.answer('Жаль((. Но если, что зовите - поиграем :). ')
+
+
+@dp.message(LetsPlayFilter())
+async def process_help_command(message: Message):
+    await message.answer('Начинаем',
+                         reply_markup=create_keyboard(board))
+
+
+@dp.callback_query(IsMoveFilter())
+async def button_1(callback: CallbackQuery):
     i = int(callback.data[0])
     j = int(callback.data[1])
     if board[i][j] == '.':
         board[i][j] = move[0]
-        print(board)
+        count[0] += 1
         if check_desk(board, move[0]):
             time.sleep(1)
             reset_board(board)
-            await callback.message.edit_text(f'Ура! Победил {move[0]}')
+            count[0] = 0
+            move[0] = 'X'
+            await callback.message.edit_text(f'Ура! Победил {move[0]}. Повторим?')
         else:
-            move[0] = 'X' if move[0] == 'O' else 'O'
-            await callback.message.edit_text(f'Ходит {move[0]}', reply_markup=create_keyboard(board))
-
+            if count[0] < 9:
+                move[0] = 'X' if move[0] == 'O' else 'O'
+                await callback.message.edit_text(f'Ходит {move[0]}', reply_markup=create_keyboard(board))
+            else:
+                time.sleep(1)
+                await callback.message.edit_text('Ничья! Победила дружба). Повторим?')
+                count[0] = 0
+                move[0] = 'X'
+                reset_board(board)
         await callback.answer()
     else:
         await callback.answer()
